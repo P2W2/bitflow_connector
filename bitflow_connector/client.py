7b0f9e30d9bf5d835831538f6fe14da25e91d5e1
peewee <peewee.manzer@outlook.de>
2020-10-01 14:12:38 +0200

    add sample encoding

--- bitflow_connector/client.py
+++ bitflow_connector/client.py
@@ -1,16 +1,30 @@
 import socket
+import time
 
 
 class Client:
     def __init__(self, host='localhost', port=8080, buffer_size=1024):
+        self.host = host
+        self.port = port
         self.buffer_size = buffer_size
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.connect((host, port))
+        self.s.connect((self.host, self.port))
+        self.connected = True
 
     def send_sample(self, sample):
-        self.s.send(sample)
-        #data = self.s.recv(self.buffer_size)
-        #return data
+        try:
+            self.s.send(sample.encode())
+        except socket.error:
+            connected = False
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("connection lost... reconnecting")
+            while not connected:
+                try:
+                    self.s.connect((self.host, self.port))
+                    connected = True
+                    print("re-connection successful")
+                except socket.error:
+                    time.sleep(1)
 
     def close_connection(self):
         self.s.close()
